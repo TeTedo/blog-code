31da52a1096f2b9d1bf0659e72bf64842edc0d4c
박태석 <qkrxotjr9508@gmail.com>
2024-01-17 10:45:55 +0900

    nginx-deploy init

---
+++ nginx-deploy/src/main/java/nonstop/nginxdeploy/NginxDeployApplication.java
@@ -0,0 +1,13 @@
+package nonstop.nginxdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxDeployApplication.class, args);
+	}
+
+}
---
+++ nginx-deploy/src/test/java/nonstop/nginxdeploy/NginxDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package nonstop.nginxdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NginxDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
